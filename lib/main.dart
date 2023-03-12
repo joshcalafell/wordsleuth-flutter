5f32385a0ee34e9606170b045082dedd1ecea35c
rabbitfighter81 <rabbitfighter81@gmail.com>
2023-03-11 23:29:31 -0800

    Reorganization of classes

--- lib/main.dart
+++ lib/main.dart
@@ -13,90 +13,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MyHomePage(title: 'Word Sleuth'),
+      home: const AuthPage(title: 'Word Sleuth'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key, required this.title});
 
   // Fields in a Widget subclass are always marked "final".
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<AuthPage> createState() => _AuthPageState();
 }
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key, required this.title});
-
-  // Fields in a Widget subclass are always marked "final".
-  final String title;
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class CreateAccountPage extends StatefulWidget {
-  const CreateAccountPage({super.key, required this.title});
-
-  // Fields in a Widget subclass are always marked "final".
-  final String title;
-
-  @override
-  State<CreateAccountPage> createState() => _CreateAccountPageState();
-}
-
-class _CreateAccountPageState extends State<CreateAccountPage> {
-  // ignore: unused_field
-  bool _isSignUpComplete = false;
-
-  bool _passwordVerifyVisible = false;
-  bool _passwordNewVisible = false;
-
-  final _signInForm = {};
-
+class _AuthPageState extends State<AuthPage> {
   @override
   initState() {
     super.initState();
+    _configureAmplify();
   }
 
-  void _toggleNewPasswordVisible() {
-    setState(() {
-      _passwordNewVisible = !_passwordNewVisible;
-    });
-  }
-
-  void _togglePasswordVerifyVisible() {
-    setState(() {
-      _passwordVerifyVisible = !_passwordVerifyVisible;
-    });
-  }
-
-  void _updateSignInForm(typedata, text) {
-    setState(() {
-      _signInForm[typedata] = text;
-    });
-  }
-
-  Future<void> signUpUser() async {
-    try {
-      final userAttributes = <CognitoUserAttributeKey, String>{
-        CognitoUserAttributeKey.email: 'email@domain.com',
-        CognitoUserAttributeKey.phoneNumber: '+15559101234',
-      };
-      final result = await Amplify.Auth.signUp(
-        username: 'myusername',
-        password: 'mysupersecurepassword',
-        options: CognitoSignUpOptions(userAttributes: userAttributes),
-      );
-      setState(() {
-        _isSignUpComplete = result.isSignUpComplete;
-      });
-    } on AuthException catch (e) {
-      safePrint(e.message);
-    }
+  void _triggerQuestion() {
+    // ...
   }
 
   Future<void> _configureAmplify() async {
@@ -111,115 +51,59 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
     }
   }
 
+  Future<bool> isUserSignedIn() async {
+    final result = await Amplify.Auth.fetchAuthSession();
+    return result.isSignedIn;
+  }
+
+  Future<AuthUser> getCurrentUser() async {
+    final user = await Amplify.Auth.getCurrentUser();
+    return user;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            ),
-            Text(
-              'Sign up for an account',
-              style: const TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 16,
-                color: Colors.black,
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title),
+              bottom: const TabBar(
+                tabs: [
+                  Tab(icon: Text('Sign In')),
+                  Tab(icon: Text('Create Account')),
+                ],
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            body: TabBarView(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                LoginPage(title: 'Sign In'),
+                CreateAccountPage(title: 'Create Account')
+              ],
             ),
-          ],
-        ),
-        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: TextField(
-              onChanged: (text) {
-                _updateSignInForm("chooseUsername", text);
-                safePrint('Chosen Username: $_signInForm["chooseUsername"]');
-              },
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Userame',
-                  labelText: 'Username'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: TextField(
-              onChanged: (text) {
-                _updateSignInForm("choosePassword", text);
-                safePrint('Chosen Password: $_signInForm["choosePassword"]');
-              },
-              obscureText: _passwordNewVisible,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                      icon: Icon(_passwordNewVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off),
-                      onPressed: _toggleNewPasswordVisible),
-                  hintText: 'New Password',
-                  labelText: 'New Password'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: TextField(
-              onChanged: (text) {
-                _updateSignInForm("confirmedPassword", text);
-                safePrint(
-                    'Confirmed Password: $_signInForm["confirmedPassword"]');
-              },
-              obscureText: _passwordVerifyVisible,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                      icon: Icon(_passwordVerifyVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off),
-                      onPressed: _togglePasswordVerifyVisible),
-                  hintText: 'Password',
-                  labelText: 'Verify Password'),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          ),
-          MaterialButton(
-            minWidth: double.tryParse('340'),
-            height: 50,
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-            onPressed: () {},
-            color: Colors.deepPurple,
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(5.0),
-            ),
-            child: Text(
-              'Create Account',
-              style: const TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 16,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          ),
-        ]),
-      ],
+            floatingActionButton: FloatingActionButton(
+                onPressed: _triggerQuestion,
+                tooltip: 'Questions',
+                backgroundColor: Colors.deepPurple,
+                child: Icon(Icons.question_answer)),
+          )),
     );
   }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key, required this.title});
+
+  // Fields in a Widget subclass are always marked "final".
+  final String title;
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
@@ -255,18 +139,6 @@ class _LoginPageState extends State<LoginPage> {
     setState(() {
       _signInForm[typedata] = text;
     });
-  }
-
-  Future<void> _configureAmplify() async {
-    final authPlugin = AmplifyAuthCognito();
-    await Amplify.addPlugin(authPlugin);
-
-    try {
-      await Amplify.configure(amplifyconfig);
-    } on AmplifyAlreadyConfiguredException {
-      safePrint(
-          "Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
-    }
   }
 
   Future<void> signInUser(String username, String password) async {
@@ -388,70 +260,174 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class CreateAccountPage extends StatefulWidget {
+  const CreateAccountPage({super.key, required this.title});
+
+  // Fields in a Widget subclass are always marked "final".
+  final String title;
+
+  @override
+  State<CreateAccountPage> createState() => _CreateAccountPageState();
+}
+
+class _CreateAccountPageState extends State<CreateAccountPage> {
+  // ignore: unused_field
+  bool _isSignUpComplete = false;
+
+  bool _passwordVerifyVisible = false;
+  bool _passwordNewVisible = false;
+
+  final _signInForm = {};
+
   @override
   initState() {
     super.initState();
-    _configureAmplify();
   }
 
-  void _triggerQuestion() {
-    // ...
+  void _toggleNewPasswordVisible() {
+    setState(() {
+      _passwordNewVisible = !_passwordNewVisible;
+    });
   }
 
-  Future<void> _configureAmplify() async {
-    final authPlugin = AmplifyAuthCognito();
-    await Amplify.addPlugin(authPlugin);
+  void _togglePasswordVerifyVisible() {
+    setState(() {
+      _passwordVerifyVisible = !_passwordVerifyVisible;
+    });
+  }
 
+  void _updateSignInForm(typedata, text) {
+    setState(() {
+      _signInForm[typedata] = text;
+    });
+  }
+
+  Future<void> signUpUser() async {
     try {
-      await Amplify.configure(amplifyconfig);
-    } on AmplifyAlreadyConfiguredException {
-      safePrint(
-          "Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
+      final userAttributes = <CognitoUserAttributeKey, String>{
+        CognitoUserAttributeKey.email: 'email@domain.com',
+        CognitoUserAttributeKey.phoneNumber: '+15559101234',
+      };
+      final result = await Amplify.Auth.signUp(
+        username: 'myusername',
+        password: 'mysupersecurepassword',
+        options: CognitoSignUpOptions(userAttributes: userAttributes),
+      );
+      setState(() {
+        _isSignUpComplete = result.isSignUpComplete;
+      });
+    } on AuthException catch (e) {
+      safePrint(e.message);
     }
-  }
-
-  Future<bool> isUserSignedIn() async {
-    final result = await Amplify.Auth.fetchAuthSession();
-    return result.isSignedIn;
-  }
-
-  Future<AuthUser> getCurrentUser() async {
-    final user = await Amplify.Auth.getCurrentUser();
-    return user;
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: DefaultTabController(
-          length: 2,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text(widget.title),
-              bottom: const TabBar(
-                tabs: [
-                  Tab(icon: Text('Sign In')),
-                  Tab(icon: Text('Create Account')),
-                ],
+    return Column(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            ),
+            Text(
+              'Sign up for an account',
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+                color: Colors.black,
               ),
             ),
-            body: TabBarView(
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                LoginPage(title: 'Sign In'),
-                CreateAccountPage(title: 'Create Account')
-              ],
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: _triggerQuestion,
-                tooltip: 'Questions',
-                backgroundColor: Colors.deepPurple,
-                child: Icon(Icons.question_answer)),
-          )),
+          ],
+        ),
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: TextField(
+              onChanged: (text) {
+                _updateSignInForm("chooseUsername", text);
+                safePrint('Chosen Username: $_signInForm["chooseUsername"]');
+              },
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Userame',
+                  labelText: 'Username'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: TextField(
+              onChanged: (text) {
+                _updateSignInForm("choosePassword", text);
+                safePrint('Chosen Password: $_signInForm["choosePassword"]');
+              },
+              obscureText: _passwordNewVisible,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                      icon: Icon(_passwordNewVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off),
+                      onPressed: _toggleNewPasswordVisible),
+                  hintText: 'New Password',
+                  labelText: 'New Password'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: TextField(
+              onChanged: (text) {
+                _updateSignInForm("confirmedPassword", text);
+                safePrint(
+                    'Confirmed Password: $_signInForm["confirmedPassword"]');
+              },
+              obscureText: _passwordVerifyVisible,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                      icon: Icon(_passwordVerifyVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off),
+                      onPressed: _togglePasswordVerifyVisible),
+                  hintText: 'Password',
+                  labelText: 'Verify Password'),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          ),
+          MaterialButton(
+            minWidth: double.tryParse('340'),
+            height: 50,
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+            onPressed: () {},
+            color: Colors.deepPurple,
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            child: Text(
+              'Create Account',
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          ),
+        ]),
+      ],
     );
   }
 }
