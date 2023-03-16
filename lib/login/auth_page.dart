1470332786be443dea8eb1aa124e621685331c84
rabbitfighter81 <rabbitfighter81@gmail.com>
2023-03-16 01:29:08 -0700

    Fake logged in templates

--- lib/login/auth_page.dart
+++ lib/login/auth_page.dart
@@ -58,37 +58,121 @@ class _AuthPageState extends State<AuthPage> {
     // ...
     String signInText = 'Sign In';
     String signUpText = 'Create Account';
+    String fabQuestionsTooltipText = 'Questions';
 
-    safePrint(' ~~~ Fin ~~~');
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: DefaultTabController(
-          length: 2,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text(widget.title),
-              bottom: TabBar(
-                tabs: [
-                  Tab(icon: Text(signInText)),
-                  Tab(icon: Text(signUpText)),
-                ],
+    return _checkAuthStatus() == true
+        ? MaterialApp(
+            theme: ThemeData(
+              primarySwatch: Colors.deepPurple,
+            ),
+            home: DefaultTabController(
+                length: 2,
+                child: Scaffold(
+                  appBar: AppBar(
+                    title: Text(widget.title),
+                    bottom: TabBar(
+                      tabs: [
+                        Tab(icon: Text(signInText)),
+                        Tab(icon: Text(signUpText)),
+                      ],
+                    ),
+                  ),
+                  body: TabBarView(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      LoginPage(title: signInText),
+                      CreateAccountPage(title: signUpText),
+                    ],
+                  ),
+                  floatingActionButton: FloatingActionButton(
+                      onPressed: () {},
+                      tooltip: fabQuestionsTooltipText,
+                      backgroundColor: Colors.deepPurple,
+                      child: Icon(Icons.question_answer)),
+                )),
+          )
+        : MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+                primarySwatch: Colors.blue, platform: TargetPlatform.iOS),
+            home: PicListPage(),
+            title: 'Title goes here...',
+          );
+  }
+}
+
+class PicListPage extends StatelessWidget {
+  const PicListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Pics List View")),
+      body: ListView.builder(
+        itemCount: _images.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: ListTile(
+              title: Text('Tile $index'),
+              leading: Hero(
+                tag: index,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(4, 16, 4, 0),
+                  child: Image.network(_images[index],
+                      width: 100, fit: BoxFit.cover),
+                ),
               ),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => PicDetailPage(heroTag: index)));
+              },
             ),
-            body: TabBarView(
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                LoginPage(title: 'Sign In'),
-                CreateAccountPage(title: 'Create Account'),
-              ],
-            ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: () {},
-                tooltip: 'Questions',
-                backgroundColor: Colors.deepPurple,
-                child: Icon(Icons.question_answer)),
-          )),
+          );
+        },
+      ),
     );
   }
 }
+
+class PicDetailPage extends StatelessWidget {
+  final int heroTag;
+  const PicDetailPage({super.key, required this.heroTag});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Pics List View Detail")),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Hero(
+                  tag: heroTag,
+                  child: Image.network(_images[heroTag]),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Text(
+              "Content goes here...",
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+final List<String> _images = [
+  'https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/273935/pexels-photo-273935.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/1591373/pexels-photo-1591373.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/462024/pexels-photo-462024.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+];
