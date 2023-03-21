a15c03e2d790be8f5a2ce6445e62f999d83985f0
rabbitfighter81 <rabbitfighter81@gmail.com>
2023-03-21 12:38:19 -0700

    Removed unnecessary hub part 2

--- lib/login/forms/form_login_account.dart
+++ lib/login/forms/form_login_account.dart
@@ -78,27 +78,6 @@ class _FormLoginAccountState extends State<FormLoginAccount> {
     return result.isSignedIn;
   }
 
-  StreamSubscription<HubEvent> hubSubscription =
-      Amplify.Hub.listen([HubChannel.Auth], (hubEvent) {
-    switch (hubEvent.eventName) {
-      case 'SIGNED_IN':
-        safePrint('USER IS SIGNED IN');
-        break;
-      case 'SIGNED_OUT':
-        safePrint('USER IS SIGNED OUT');
-        break;
-      case 'SESSION_EXPIRED':
-        safePrint('SESSION HAS EXPIRED');
-        break;
-      case 'USER_DELETED':
-        safePrint('USER HAS BEEN DELETED');
-        break;
-      default:
-        safePrint(hubEvent.eventName);
-        break;
-    }
-  });
-
   @override
   Widget build(BuildContext context) {
     return !_isSignedIn
