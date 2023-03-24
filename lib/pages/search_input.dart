2b1a11b1e9c2ccf5d4af86fa6105f405d793f906
rabbitfighter81 <rabbitfighter81@gmail.com>
2023-03-24 14:15:48 -0700

    Sign Out Button added to dawer and other drawer fixes

--- lib/pages/search_input.dart
+++ lib/pages/search_input.dart
@@ -1,5 +1,7 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:easy_search_bar/easy_search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:word_sleuth/login/tabs/tabs_auth.dart';
 
 class SearchInput extends StatefulWidget {
   const SearchInput({Key? key}) : super(key: key);
@@ -11,7 +13,7 @@ class SearchInput extends StatefulWidget {
 class _SearchInputState extends State<SearchInput> {
   String searchValue = '';
   final List<String> _suggestions = [
-    'Afeganistan',
+    'Afganistan',
     'Albania',
     'Algeria',
     'Australia',
@@ -20,8 +22,18 @@ class _SearchInputState extends State<SearchInput> {
     'Madagascar',
     'Mozambique',
     'Portugal',
+    'Taian',
     'Zambia'
   ];
+
+  Future<void> signOutUser() async {
+    try {
+      final result = await Amplify.Auth.signOut().then((value) => null);
+      safePrint(result);
+    } on AuthException catch (e) {
+      safePrint(e.message);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +42,9 @@ class _SearchInputState extends State<SearchInput> {
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: Scaffold(
           appBar: EasySearchBar(
-              title: const Center(child: Text('Serach for Anagrams')),
+
+              // isFloating: true,
+              title: const Text('Search for Anagrams'),
               foregroundColor: Colors.white,
               backgroundColor: Colors.deepPurple,
               openOverlayOnSearch: true,
@@ -42,24 +56,78 @@ class _SearchInputState extends State<SearchInput> {
                   }),
               suggestions: _suggestions),
           drawer: Drawer(
-              child: ListView(padding: const EdgeInsets.all(0), children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.deepPurple,
-              ),
-              child: Text(
-                'Pages',
-              ),
-            ),
-            ListTile(
-                textColor: Colors.black,
-                title: const Text('Item 1'),
-                onTap: () => {}),
-            ListTile(
-                textColor: Colors.black,
-                title: const Text('Item 2'),
-                onTap: () => {}),
-          ])),
+              child: ListView(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  children: [
+                // ignore: prefer_const_constructors
+                SizedBox(
+                  height: 100.0,
+                  child: const DrawerHeader(
+                      decoration: BoxDecoration(color: Colors.deepPurple),
+                      padding: EdgeInsets.all(8),
+                      child: Text('Categories',
+                          style: TextStyle(color: Colors.white))),
+                ),
+                ListTile(
+                    textColor: Colors.black,
+                    title: const Text('Item 1'),
+                    onTap: () => {safePrint('Item 1')}),
+                ListTile(
+                    textColor: Colors.black,
+                    title: const Text('Item 2'),
+                    onTap: () => {safePrint('Item 2')}),
+                ListTile(
+                    textColor: Colors.black,
+                    title: const Text('Item 3'),
+                    onTap: () => {safePrint('Item 3')}),
+                ListTile(
+                    textColor: Colors.black,
+                    title: const Text('Item 4'),
+                    onTap: () => {safePrint('Item 4')}),
+                ListTile(
+                    textColor: Colors.black,
+                    title: const Text('Item 5'),
+                    onTap: () => {safePrint('Item 5')}),
+                Column(
+                  children: [
+                    const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+                    ),
+                    MaterialButton(
+                      minWidth: double.tryParse('200'),
+                      padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                      onPressed: () {
+                        // Validate returns true if the form is valid, or false otherwise.
+                        signOutUser().then((value) => Navigator.of(context)
+                            .pushAndRemoveUntil(
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const TabsAuth(title: 'Word Sleuth')),
+                                (route) => false));
+
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              content: Center(child: Text('Signing Out...'))),
+                        );
+                      },
+                      color: Colors.deepPurple,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      child: const Text(
+                        'Sign Out',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ])),
           body: Center(
               child: Text(
             'Value: $searchValue',
